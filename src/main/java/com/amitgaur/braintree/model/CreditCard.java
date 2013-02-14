bb81338407cfe07abd7f47fa862d08603cd9672e
Your Name <amitgaur@gmail.com>
2013-02-14 12:42:34 -0800

    Redo Model

---
+++ src/main/java/com/amitgaur/braintree/model/CreditCard.java
@@ -0,0 +1,11 @@
+package com.amitgaur.braintree.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: amitgaur
+ * Date: 2/14/13
+ * Time: 12:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CreditCard {
+}
